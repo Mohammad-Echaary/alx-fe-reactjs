d6121b3c8af241077d96a10f42f5dfe63c1833ff
Mohammad Echaary <mohammadechaary@gmail.com>
2024-08-05 10:54:13 +0100

    new commit

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -5,6 +5,7 @@ import WelcomeMessage from "./components/WelcomeMessage";
 import Header from "./components/Header";
 import MainContent from "./components/MainContent";
 import Footer from "./components/Footer";
+import UserProfile from "./components/UserProfile";
 import "./App.css";
 
 function App() {
@@ -33,9 +34,10 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
       <WelcomeMessage />
-      <Header></Header>
-      <MainContent></MainContent>
-      <Footer></Footer>
+      <Header />
+      <MainContent />
+      <Footer />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
     </>
   );
 }
