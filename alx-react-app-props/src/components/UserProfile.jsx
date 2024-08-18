5c226a532f69a74d6c58bc47ce30a021379fc16e
Mohammad Echaary <mohammadechaary@gmail.com>
2024-08-18 20:28:03 +0100

    new commit

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,7 +1,16 @@
-import UserInfo from "./UserInfo";
+import { useContext } from "react";
+import UserContext from "../UserContext";
 
-function ProfilePage({ userData }) {
-  return <UserInfo userData={userData} />;
+function UserProfile() {
+  const userData = useContext(UserContext);
+
+  return (
+    <div>
+      <h1>User Profile</h1>
+      <p>Name: {userData.name}</p>
+      <p>Email: {userData.email}</p>
+    </div>
+  );
 }
 
-export default ProfilePage;
+export default UserProfile;
