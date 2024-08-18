b638f2369f33f1d9c46cd0d69d8cef276c06e80c
Mohammad Echaary <mohammadechaary@gmail.com>
2024-08-18 20:14:47 +0100

    new commit

---
+++ alx-react-app-props/src/UserContext.js
@@ -0,0 +1,18 @@
+import { createContext, useState } from "react";
+
+// Create a context with a default value
+const UserContext = createContext(null);
+
+// Create a provider component
+export const UserProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+// Export the context
+export default UserContext;
