11fb68c582a06c8ba2598fad8772f85a403490c2
Mohammad Echaary <mohammadechaary@gmail.com>
2024-09-03 09:57:21 +0100

    new commit

---
+++ form-handling-react/form-handling-react/src/components/RegistrationForm.jsx
@@ -0,0 +1,14 @@
+import RegistrationForm from "./RegistrationForm";
+import FormikForm from "./FormikForm";
+
+const Register = () => {
+  return (
+    <div>
+      <h1>Register</h1>
+      <RegistrationForm />
+      <FormikForm />
+    </div>
+  );
+};
+
+export default Register;
