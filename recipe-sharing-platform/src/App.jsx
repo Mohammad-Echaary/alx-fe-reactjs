839081ff4f1eda98f7a68d9ffff4f77feab7c98b
Mohammad Echaary <mohammadechaary@gmail.com>
2024-09-09 10:53:34 +0100

    new commit

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import HomePage from "./components/HomePage";
 
 function App() {
-  return <h1 className="text-blue-500">Hello World</h1>;
+  return (
+    <>
+      <HomePage />
+    </>
+  );
 }
 
 export default App;
