a2ef2f5a0b631303bea1eda5deb0707f9e7d9502
Mohammad Echaary <mohammadechaary@gmail.com>
2024-09-20 11:01:42 +0100

    new commit

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,10 +1,7 @@
+import Header from "./components/Header";
+
 function App() {
-  return (
-    <div>
-      <h1>GitHub User Search</h1>
-      <p>Enter a GitHub username to see details</p>
-    </div>
-  );
+  return <Header />;
 }
 
 export default App;
