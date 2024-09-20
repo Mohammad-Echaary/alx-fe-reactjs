f1d140d266fc8a00c3f7f2a03e54fb51391f8849
Mohammad Echaary <mohammadechaary@gmail.com>
2024-09-20 11:32:45 +0100

    new commit

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://api.github.com/search/users";
+const BASE_URL = "https://api.github.com/search/users?q";
 
 export const fetchAdvancedUserData = async (username, location, minRepos) => {
   // Build the query string for the API call
