464a57c5f50551f08dea074049558db2ce70005a
Mohammad Echaary <mohammadechaary@gmail.com>
2024-09-20 11:31:07 +0100

    new commit

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -3,19 +3,25 @@ import axios from "axios";
 const BASE_URL = "https://api.github.com/search/users";
 
 export const fetchAdvancedUserData = async (username, location, minRepos) => {
-  let query = username ? `${username}+in:login` : "";
+  // Build the query string for the API call
+  let query = "";
+
+  if (username) {
+    query += `${username} in:login`;
+  }
 
   if (location) {
-    query += `+location:${location}`;
+    query += ` location:${location}`;
   }
 
   if (minRepos) {
-    query += `+repos:>=${minRepos}`;
+    query += ` repos:>=${minRepos}`;
   }
 
   try {
+    // Make the API call using Axios
     const response = await axios.get(`${BASE_URL}?q=${query}`);
-    return response.data; // Return the user data
+    return response.data; // Return the response data
   } catch (error) {
     throw new Error("Error fetching users");
   }
