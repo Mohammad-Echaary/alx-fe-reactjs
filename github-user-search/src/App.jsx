17f2c0993ee3c2d3d2209083aaa27f0520b7460f
Mohammad Echaary <mohammadechaary@gmail.com>
2024-09-20 11:11:00 +0100

    new commit

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,11 +1,42 @@
 import Header from "./components/Header";
 import Search from "./components/Search";
+
+import { useState } from "react";
+import { fetchGitHubUser } from "./services/githubService";
+
 function App() {
+  const [username, setUsername] = useState("");
+  const [userData, setUserData] = useState(null);
+
+  const handleSearch = async () => {
+    try {
+      const data = await fetchGitHubUser(username);
+      setUserData(data);
+    } catch (error) {
+      console.error("User not found or error occurred");
+    }
+  };
+
   return (
     <div>
-      <h1>GitHub User Search</h1>
       <Header />
       <Search />
+      <input
+        type="text"
+        placeholder="Enter GitHub username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <button onClick={handleSearch}>Search</button>
+
+      {userData && (
+        <div>
+          <h2>{userData.name}</h2>
+          <p>{userData.bio}</p>
+          <p>Followers: {userData.followers}</p>
+          <p>Following: {userData.following}</p>
+        </div>
+      )}
     </div>
   );
 }
